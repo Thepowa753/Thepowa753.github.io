e0c9f3ee7875ccb51417fcc861398e02817774a5
Federico Bregant <federico.bregant@mquadro.net>
2022-07-13 11:24:18 +0200

    feat: better style

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Thepowa753.github.io
+- ToDo (add credit to vecteezy.com for BG)
--- js/main.js
+++ js/main.js
@@ -2,5 +2,7 @@ $(".w3-bar").click(function(){
 	$(".w3-bar").animate({height: "10%"}, "slow");
 });
 function change_page(page){
+	$("#pager").animate({height: "0%", opacity: "0"}, "1s");
 	$("#pager").load(page+".html");
+	$("#pager").animate({height: "93%", opacity: "1"}, "1s");
 }
